11de21a0783eba5aff402d281b954f9fb22a5af8
alejandro <alejandro.mfp@live.com>
2024-04-28 00:18:24 +0200

    peticion vacaciones y activity para gestionarlas

--- TaskSphere/app/src/main/java/com/example/tasksphere/CalendarActivity.java
+++ TaskSphere/app/src/main/java/com/example/tasksphere/CalendarActivity.java
@@ -4,23 +4,47 @@ import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
 import java.text.DateFormatSymbols;
+import java.util.Map;
 
 public class CalendarActivity extends AppCompatActivity {
 
     TextView fecha;
+    private FirebaseAuth mAuth;
+
+    private Calendar fechaSeleccionada = Calendar.getInstance();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Inicializa Firebase
+        FirebaseApp.initializeApp(this);
+        // Configura Firestore
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        mAuth = FirebaseAuth.getInstance();
 
         // Cambiar la localización de la app a español
         Locale locale = new Locale("es", "ES");
@@ -33,7 +57,69 @@ public class CalendarActivity extends AppCompatActivity {
 
         fecha = findViewById(R.id.fecha);
         actualizarFechaTexto(new Date()); // Actualizar la fecha al iniciar
+
+        Button botonVacaciones = findViewById(R.id.botonVacaciones);
+        botonVacaciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enviarSolicitudVacaciones();
+            }
+        });
+
+
+
     }
+
+    private void enviarSolicitudVacaciones() {
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null) {
+            Toast.makeText(this, "No autenticado, no se puede enviar la solicitud", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String fechaVacaciones = sdf.format(fechaSeleccionada.getTime());
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        // Comprobar si existe una solicitud similar
+        db.collection("solicitudes")
+                .whereEqualTo("usuario", user.getUid())
+                .whereEqualTo("fecha", fechaVacaciones)
+                .whereEqualTo("estado", "pendiente")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && !task.getResult().isEmpty()) {
+                        Toast.makeText(CalendarActivity.this, "Ya existe una solicitud para esta fecha.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        enviarNuevaSolicitud(db, user.getUid(), fechaVacaciones);
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(CalendarActivity.this, "Error al verificar solicitudes existentes", Toast.LENGTH_SHORT).show();
+                });
+    }
+
+
+    private void enviarNuevaSolicitud(FirebaseFirestore db, String userId, String fechaVacaciones) {
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("usuario", userId);
+        userData.put("fecha", fechaVacaciones);
+        userData.put("estado", "pendiente");
+
+        db.collection("solicitudes")
+                .add(userData)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(CalendarActivity.this, "Solicitud enviada con éxito", Toast.LENGTH_LONG).show();
+                    Log.d("Firestore", "Documento añadido con ID: " + documentReference.getId());
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(CalendarActivity.this, "Error al enviar la solicitud", Toast.LENGTH_LONG).show();
+                    Log.w("Firestore", "Error añadiendo documento", e);
+                });
+    }
+
 
     public void abrirCalendario(View view){
         Calendar cal = Calendar.getInstance();
@@ -45,7 +131,6 @@ public class CalendarActivity extends AppCompatActivity {
         DatePickerDialog dpd = new DatePickerDialog(CalendarActivity.this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                Calendar fechaSeleccionada = Calendar.getInstance();
                 fechaSeleccionada.set(year, month, dayOfMonth);
                 actualizarFechaTexto(fechaSeleccionada.getTime());
             }
@@ -79,5 +164,7 @@ public class CalendarActivity extends AppCompatActivity {
             fecha.setText(sdf.format(date));
         }
     }
+
+
 }
 
